1c195bb41b70e03d4368b09440327612115634da
ShahKamran <kamran@kamranshah.com>
2021-01-14 15:07:48 +0000

    Added user profile

--- modules/iam-user/main.tf
+++ modules/iam-user/main.tf
@@ -1,3 +1,4 @@
+# Create IAM User account
 resource "aws_iam_user" "usr" {
   name = var.name
   path = "/system/"
@@ -7,47 +8,106 @@ resource "aws_iam_user" "usr" {
   }
 }
 
+# Create IAM Access Key
 resource "aws_iam_access_key" "usr" {
-  user = aws_iam_user.usr.name
-  pgp_key = "keybase:kamran"
+  user    = aws_iam_user.usr.name
+  pgp_key = "keybase:someuserthatexists"
 }
 
-resource "aws_iam_user_policy" "usr_ro" {
+# Create Login Profile
+resource "aws_iam_user_login_profile" "usr" {
+  user    = aws_iam_user.usr.name
+  pgp_key = "keybase:someuserthatexists"
+}
+
+# Attach Permission Policy to User Account
+resource "aws_iam_user_policy" "usr_s3_restricted" {
   name = "S3access-specific-bucket"
   user = aws_iam_user.usr.name
 
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "s3:GetBucketLocation",
-            "Resource": "arn:aws:s3:::winnershawsk8gurus2021*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": "s3:ListBucket",
-            "Resource": "arn:aws:s3:::winnershawsk8gurus2021*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": "s3:ListAllMyBuckets",
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": "s3:*",
-            "Resource": [
-                "arn:aws:s3:::winnershawsk8gurus2021*"
-                ],
-            "Condition": {
-                "StringEquals": {
-                    "aws:RequestedRegion": "eu-west-2"
-                }
-            }
-        }
-    ]
+  # This section uses data block for json policy definition
+
+  policy = data.aws_iam_policy_document.usr_s3_restricted.json
+
+  # This section uses in-line policy statement rather than above data block json policy
+
+  #
+  #  policy = <<EOF
+  #{
+  #    "Version": "2012-10-17",
+  #    "Statement": [
+  #        {
+  #            "Effect": "Allow",
+  #            "Action": "s3:GetBucketLocation",
+  #            "Resource": "arn:aws:s3:::winnershawsk8gurus2021*"
+  #        },
+  #        {
+  #            "Effect": "Allow",
+  #            "Action": "s3:ListBucket",
+  #            "Resource": "arn:aws:s3:::winnershawsk8gurus2021*"
+  #        },
+  #        {
+  #            "Effect": "Allow",
+  #            "Action": "s3:ListAllMyBuckets",
+  #            "Resource": "*"
+  #        },
+  #        {
+  #            "Effect": "Allow",
+  #            "Action": "s3:*",
+  #            "Resource": [
+  #                "arn:aws:s3:::winnershawsk8gurus2021*"
+  #                ],
+  #            "Condition": {
+  #                "StringEquals": {
+  #                    "aws:RequestedRegion": "eu-west-2"
+  #                }
+  #            }
+  #        }
+  #    ]
+  #}
+  #EOF
 }
-EOF
-}  
+
+data "aws_iam_policy_document" "usr_s3_restricted" {
+  statement {
+    sid    = "allowgetbucketlistv1"
+    effect = "Allow"
+    actions = [
+      "s3:GetBucketLocation",
+      "s3:ListBucket",
+    ]
+    resources = [
+      "arn:aws:s3:::winnershawsk8gurus2021*",
+    ]
+  }
+
+  statement {
+    sid    = "allowlistallbucketsv1"
+    effect = "Allow"
+    actions = [
+      "s3:ListAllMyBuckets",
+    ]
+    resources = [
+      "*",
+    ]
+  }
+
+  statement {
+    sid    = "allowregionalbucketactionsv1"
+    effect = "Allow"
+    actions = [
+      "s3:*"
+    ]
+    resources = [
+      "arn:aws:s3:::winnershawsk8gurus2021*",
+    ]
+    condition {
+      test     = "StringEquals"
+      variable = "aws:RequestedRegion"
+      values = [
+        "eu-west-2",
+      ]
+    }
+  }
+}
+
---
+++ output.tf
@@ -0,0 +1,15 @@
+output "user_name" {
+ value = module.iam-user.user_name
+}
+
+output "user_id" {
+ value = module.iam-user.user_id
+}
+
+output "password" {
+ value = module.iam-user.password
+}
+
+output "secret" {
+ value = module.iam-user.secret
+}
